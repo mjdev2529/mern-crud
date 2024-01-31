396c17d1fa001acd8b70741af52feb7158bf420a
Miljan Jonota <mjdev2529@gmail.com>
2024-01-31 22:37:23 +0800

    create sign up api

--- api/index.js
+++ api/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000);
+
+// routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
