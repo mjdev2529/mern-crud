f1c0d3940fd4a2edb4a5f5bb3480da61e2191295
Miljan Jonota <mjdev2529@gmail.com>
2024-01-31 17:08:53 +0800

    set up db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected");
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Server is listening to Port 3000");
-});
+// app.listen(3000, ()=>{
+//     console.log("Server is listening to Port 3000");
+// });
