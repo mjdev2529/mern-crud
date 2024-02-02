96f41dd1134cbb78027ed648f107f0baa8355013
Miljan Jonota <mjdev2529@gmail.com>
2024-02-03 04:17:11 +0800

    complete profile page ui

--- web/src/pages/Profile.js
+++ web/src/pages/Profile.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {};
+
+  const inputField = (e) => {};
+
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <img
+          src={currentUser.img_url}
+          alt="User profile"
+          className="h-24 w-24 rounded-full self-center object-cover cursor-pointer"
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={inputField}
+          defaultValue={currentUser.username}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={inputField}
+          defaultValue={currentUser.email}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={inputField}
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? "loading..." : "update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Signout</span>
+      </div>
+    </div>
+  );
 }
